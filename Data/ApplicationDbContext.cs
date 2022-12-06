bdc1289f5df906297ca855546ca33b44d74a5710
fxop0218 <francescoli0218@gmail.com>
2022-12-06 13:10:39 +0100

    ApplicationDbContext created

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
