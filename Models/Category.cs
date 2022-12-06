378308eaeeb6e8d088358f5f982f53813c2944e3
fxop0218 <francescoli0218@gmail.com>
2022-12-06 12:57:16 +0100

    first commit

---
+++ Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+
+namespace BookWeb.Models
+{
+    public class Category
+    {
+        [Key] // Primary key
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDataTime { get; set; } = DateTime.Now; 
+
+    }
+}
